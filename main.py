cf178a5afccad42b20907f22c51937e6be07cb5b
vinhngo380 <vinhngo380@gmail.com>
2023-09-12 16:04:13 -0400

    added feature to move files based on name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 import os, time, shutil, re
 global DEBUG
-DEBUG = False
+DEBUG = True
 
 
 class FileTypeMove:
@@ -25,12 +25,12 @@ class FileTypeMove:
 
     def create_file_list(self, files):
         self.files_list = list(filter(self.base_filter, files))
-        if DEBUG: print(self.files_list)
+        if DEBUG: print(self.name, self.files_list)
 
     def create_new_directory(self):
         if not os.path.isdir(self.target_directory + '/'):
             os.mkdir(self.target_directory)
-            if DEBUG: print("downloaded", self.name, "made")
+            if DEBUG: print(self.name, "directory made")
 
     def move_files(self, files):
         self.create_file_list(files)
@@ -39,13 +39,20 @@ class FileTypeMove:
             for file in self.files_list:
                 new_path = self.target_directory + '/' + file
                 shutil.move(self.download_directory + '/' + file, new_path)
-                if DEBUG: print("moved", file, "from", self.download_directory, "to", new_path)
+                if DEBUG: print(self.name, "moved", file, "from", self.download_directory, "to", new_path)
 
     def get_list(self):
         return self.files_list
 
-
-
+    def rename(self):
+        files = os.listdir(self.target_directory)
+        self.create_file_list(files)
+        if DEBUG: print("list files in", self.target_directory, self.files_list)
+        for file in self.files_list:
+            if DEBUG: print("rename", file[len(self.conditions[0])::])
+            file_directory = self.target_directory + '/' + file
+            new_name_file_directory = self.target_directory + '/' + file.replace(self.conditions[0], '')
+            os.rename(file_directory, new_name_file_directory)
 
 
 
@@ -54,28 +61,30 @@ def moooove():
     if DEBUG: print(os.listdir())
     files = [file for file in os.listdir(downloadDirectory)]
 
+    classwork = FileTypeMove(['__classwork__'], 'classwork', '', downloadDirectory)
+    classwork.move_files(files)
+    classwork.rename()
+
     images = FileTypeMove(['.png', '.jpg', 'jpeg', 'webp'], 'images', '', downloadDirectory)
-    if DEBUG: print(images.get_list())
     images.move_files(files)
 
     documents = FileTypeMove(['.docx', '.pdf', '.xls'], 'documents', '', downloadDirectory)
-    if DEBUG: print(documents.get_list())
     documents.move_files(files)
 
-    classwork = FileTypeMove(['__classwork__'], 'classwork', downloadDirectory)
-    if DEBUG: print(classwork)
-    classwork.move_files(files)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #mac downloadDirectory = '/Users/vinhngo/Downloads'
     #windows downloadDirectory = 'C:/Users/vinh-school/Downloads'
-    downloadDirectory = '/Users/vinhngo/Downloads'
+    downloadDirectory = 'C:/Users/vinh-school/Downloads'
+    moooove()
+
+    """
     while(True):
         moooove()
         if DEBUG: print("mooove")
         time.sleep(5)
-
+    """
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
