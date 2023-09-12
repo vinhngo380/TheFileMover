1dcc65f6ffff11312a222d821958a82c260ae075
vinhngo380 <vinhngo380@gmail.com>
2023-09-11 22:38:16 -0400

    made some coding ducmentation and added debug conditionals to print statements

--- main.py
+++ main.py
@@ -5,7 +5,11 @@
 
 import os, time, shutil, re
 
-
+DEBUG = True
+"""
+methods to be used with the built in filter() function to filter out the filetypes from the download folder
+>>>list(filter(filter_method, files_from_somewhere)) --> give the files with specified extension
+"""
 def image_filter(file):
     image_filetypes = ['.jpg', '.png']
     for file_type in image_filetypes:
@@ -22,33 +26,36 @@ def document_filter(file):
     return False
 
 
+"""
+"main" method to identify and move files around
+"""
 def moooove():
     files = [file for file in os.listdir(downloadDirectory)]
 
     images = list(filter(image_filter, files))
     documents = list(filter(document_filter, files))
-    print("images", images)
-    print("doucments", documents)
+    if DEBUG: print("images", images)
+    if DEBUG: print("doucments", documents)
 
-    print(os.listdir())
+    if DEBUG: print(os.listdir())
     parent_directory = 'C:/Users/vinh-school/PycharmProjects/TheFileMover/'
     if images:
         if not os.path.isdir(parent_directory + 'downloaded_images/'):
             os.mkdir(parent_directory + 'downloaded_images')
-            print("downloaded_images made")
+            if DEBUG: print("downloaded_images made")
         for image in images:
             new_path = parent_directory + 'downloaded_images/' + image
             shutil.move(downloadDirectory + '/' + image, new_path)
-            print("moved", image, "to", new_path)
+            if DEBUG: print("moved", image, "to", new_path)
 
     if documents:
         if not os.path.isdir(parent_directory + 'downloaded_documents/'):
             os.mkdir(parent_directory + 'downloaded_documents')
-            print("downloaded_documents made")
+            if DEBUG: print("downloaded_documents made")
         for document in documents:
             new_path = parent_directory + 'downloaded_documents/' + document
             shutil.move(downloadDirectory + '/' + document, new_path)
-            print("moved", document, "to", new_path)
+            if DEBUG: print("moved", document, "to", new_path)
 
 
 # Press the green button in the gutter to run the script.
@@ -56,7 +63,7 @@ if __name__ == '__main__':
     downloadDirectory = 'C:/Users/vinh-school/Downloads'
     while(True):
         moooove()
-        print("mooove")
+        if DEBUG: print("mooove")
         time.sleep(5)
 
 
