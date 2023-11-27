09d791e80a8aae83e88d80830418e97b9365580c
vinhngo <vinhngo380@gmail.com>
2023-11-27 18:13:58 -0500

    Using pathlib to replace some functions, also fixed the add functions

--- FileMover/FileMovers.py
+++ FileMover/FileMovers.py
@@ -1,6 +1,7 @@
 import os, shutil
 from typing import List, Tuple
-from pathlib import PurePath
+from pathlib import PurePath, Path
+from glob import glob
 
 
 global DEBUG
@@ -10,25 +11,28 @@ IMAGES = ['png', 'jpeg', 'jpg', 'webp', 'svg']
 DOCUMENTS = ['docx']
 EXECS = ['msi', 'exe', 'dmg']
 
+
 class FileMover:
     #'.' is not a valid format as of right now because of filter_by_type
     def __init__(self, 
                  folder_name: str, 
                  source_directory: PurePath,
                  target_directory: PurePath,
-                 file_types: List[List[str]],
-                 valid_file_name: List[str],
-                 formatter ='_', 
+                 valid_file_types: List[List[str]],
+                 valid_file_names: List[str],
+                 formatter='_', 
                  sub_dir=[]
                  ):
         self.folder_name = folder_name
         self.source_directory = source_directory
         self.target_directory = PurePath(os.path.normpath(os.path.join(target_directory, folder_name)))
         self.files_list = []
-        self.file_types = self.add_file_types(file_types)
+        self.valid_file_types = []
+        self.add_file_types(valid_file_types)
         self.formatter = formatter
         self.sub_dir = sub_dir
-        self.valid_file_names = self.add_file_types(valid_file_name)
+        self.valid_file_names = []
+        self.add_valid_names(valid_file_names)
 
     def string_to_tuple(self, dir: str) -> Tuple[str]:
         if DEBUG: print(f"directory_to_list: {os.path.normpath(dir)}")
@@ -40,17 +44,12 @@ class FileMover:
         return path_object_dir
     
     def create_new_directories(self, directory: PurePath) -> None:
+        temp = Path(directory)
+        temp.mkdir(parents=True, exist_ok=True)
         print(f"create_new_directories: {directory}")
-        path_list = directory.parents
-        for i in range(len(path_list) - 2, -1, -1): #make this more dynamic
-            current_path = PurePath(path_list[i])
-            print(str(current_path))
-            if not os.path.isdir(current_path):
-                os.mkdir(current_path)
-        if not os.path.isdir(directory):
-            os.mkdir(directory)
 
     def filter_by_name(self, files: List[str]) -> List[str]:
+        #use glob.glob(valid_file_name.*?)
         files_filtered = []
         for file in files:
             file_name_list = file.split(self.formatter)[0]
@@ -60,29 +59,27 @@ class FileMover:
         return files_filtered
     
     def filter_by_type(self, files: List[str]) -> List[str]:
+        #use glob.glob(*.file_type?)
         files_filtered = []
         for file in files:
             file_type = file.split('.')[-1].lower()
-            if DEBUG: print(f"file = {file} file_type = {file_type} file_type type = {type(file_type)} conditional: {file_type in flattened_valid_file_types}")
-            if file_type in self.file_types:
+            if DEBUG: print(f"file = {file} file_type = {file_type} file_type type = {type(file_type)} conditional: {file_type in self.valid_file_names}")
+            if file_type in self.valid_file_names:
                 files_filtered.append(file)
         return files_filtered
     
     def filter_all(self, files) -> None:
+        #use glob.glob (valid_file.name[formatter]*.file_type?)
         temp = self.filter_by_name(files)
         if DEBUG: print(f"filter_all: filtered by name = {temp}")
         self.files_list = self.filter_by_type(temp)
-
-    def dir_from_file(self, file: str) -> str:
-        return file.split(self.formatter)[1::-2]
     
     def move_file(self, file: str) -> None:
-        file_dir = ''
-        # file_dir = self.dir_from_file(file)
-        # self.create_new_directories(file_dir)
         new_path = os.path.join(self.target_directory, file)
         print(f"move_file: source_dir = {self.source_directory} type = {type(self.source_directory)}", *self.source_directory.parts)
-        file_path = PurePath(os.path.normpath(os.path.join(*self.source_directory.parts, file)))
+        unformatted_path = os.path.join(*self.source_directory.parts, file)
+        formatted_path = os.path.normpath(unformatted_path)
+        file_path = PurePath(formatted_path)
         if DEBUG: print(f"move_file: folder_name = {self.folder_name} file = {file} paths = {file_path} to {new_path}")
         shutil.move(file_path, new_path)
         if DEBUG: print(f"{self.folder_name}: moved {file} from {file_path} to {new_path}")
@@ -96,14 +93,17 @@ class FileMover:
             self.move_file(file)
 
     def add_file_types(self, new_file_types: List[str]) -> None:
-        # if not isinstance(new_file_types, List):
-        #     pass
         for element in new_file_types:
-            if isinstance(element, list):
-                for file_type in element:
-                    self.file_types.append(file_type)
+            print(f"add file types: {element, new_file_types, not isinstance(element, List)}")
+            if not isinstance(element, List):
+                self.valid_file_types.append(element)
             else:
-                self.file_types.append(element)
+                self.add_file_types(element)
 
-    def add_valid_name(self, new_file_names: List[str]) -> None:
-        self.valid_file_names.append(new_file_names)
+    def add_valid_names(self, new_file_names: List[str]) -> None:
+        for element in new_file_names:
+            print(f"add file types: {element, new_file_names, not isinstance(element, List)}")
+            if not isinstance(element, List):
+                self.valid_file_names.append(element)
+            else:
+                self.add_valid_names(element)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import time, os
-from FileMover.FileMovers import FileMover as FileMover, IMAGES
+from FileMover.FileMovers import FileMover as FileMover, IMAGES, DOCUMENTS, EXECS
 from pathlib import Path, PurePath
 
 DOWNLOAD_DIRECTORY = str(Path.home() / "Downloads") #C:\Users\user_name\Downlaods
@@ -11,9 +11,15 @@ DEBUG = True
 
 if __name__ == '__main__':
     test_path = PurePath('/Users/Vinh/Documents/Test 1/Test2/He He He Ha/')
+    # test = FileMover('test_folder', PurePath(DOWNLOAD_DIRECTORY), test_path, [IMAGES], ['tester'])
+    # print(test.target_directory, type(test.target_directory))
+    # test.create_new_directories(test.target_directory)
+    # test.move_files()
 
+    testing_path = Path(PurePath(os.path.join(DOCUMENTS_DIRECTORY, 'Test 1/Test2/He He He Ha/')))
+    testing_path.mkdir(parents=True, exist_ok=True)
+    print("walk completed")
+    print(f"images, execs, and documents {[IMAGES, EXECS, DOCUMENTS]}")
     test = FileMover('test_folder', PurePath(DOWNLOAD_DIRECTORY), test_path, [IMAGES], ['tester'])
-    print(test.target_directory, type(test.target_directory))
-    test.create_new_directories(test.target_directory)
-    test.move_files()
+
 
